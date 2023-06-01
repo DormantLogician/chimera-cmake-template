b2310cecfdaf68894025ce0c0f28eceff707c4e7
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-05-31 20:07:23 -0400

    Modify test target template with remodeled examples.
    This changes the testing methodology used by default.

--- template/app/test_target/all_lib_target_specs/cl/specs_unitname_cl/specs_unitname_cl.cpp
+++ template/app/test_target/all_lib_target_specs/cl/specs_unitname_cl/specs_unitname_cl.cpp
@@ -1,10 +1,26 @@
 #include <boost/test/unit_test.hpp>
+#include <array>
 
 BOOST_AUTO_TEST_SUITE(unitname_cl);
 
-BOOST_AUTO_TEST_CASE(First_spec_description)
+enum class Situation
 {
-    BOOST_FAIL("Incomplete test case.");
+    First,
+    Second,
+    Third,
+    Nth
+};
+
+BOOST_AUTO_TEST_CASE(test_case_1)
+{
+    constexpr std::array sit{
+        Situation::First,
+        Situation::Second,
+        Situation::Third,
+        Situation::Nth
+    };
+
+    BOOST_FAIL("Test case body not completed yet.");
 };
 
 BOOST_AUTO_TEST_SUITE_END();
