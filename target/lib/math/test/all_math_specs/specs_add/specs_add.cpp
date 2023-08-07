d48211c375db09558c73c0e1878779e90023e10a
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-08-06 23:15:38 -0400

    Remodel testing methodologies shown in example 'math' library and under 'template' folder.

--- target/lib/math/test/all_math_specs/specs_add/specs_add.cpp
+++ target/lib/math/test/all_math_specs/specs_add/specs_add.cpp
@@ -5,122 +5,25 @@
 
 BOOST_AUTO_TEST_SUITE(add);
 
-enum class Situation
-{
-    Greater_left_argument,
-    Left_argument_is_zero,
-    Right_argument_is_zero,
-    Has_negative_numbers,
-    Has_positive_numbers
-};
+
+BOOST_AUTO_TEST_SUITE(Output_is_sum_of_both_numbers);
 
 BOOST_AUTO_TEST_CASE(test_case_1)
 {
-    constexpr std::array sit{
-        Situation::Left_argument_is_zero,
-        Situation::Right_argument_is_zero
-    };
-
     BOOST_CHECK_EQUAL(math::add(0, 0), 0);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_2)
-{
-    constexpr std::array sit{
-        Situation::Left_argument_is_zero,
-        Situation::Has_negative_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(0, -1), -1);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_3)
-{
-    constexpr std::array sit{
-        Situation::Left_argument_is_zero,
-        Situation::Has_positive_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(0, 2), 2);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_4)
-{
-    constexpr std::array sit{
-        Situation::Right_argument_is_zero,
-        Situation::Has_negative_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(-1, 0), -1);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_5)
-{
-    constexpr std::array sit{
-        Situation::Right_argument_is_zero,
-        Situation::Has_positive_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(2, 0), 2);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_6)
-{
-    constexpr std::array sit{
-        Situation::Has_negative_numbers,
-        Situation::Has_positive_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(-2, 3), 1);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_7)
-{
-    constexpr std::array sit{
-        Situation::Greater_left_argument,
-        Situation::Has_negative_numbers,
-        Situation::Has_positive_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(3, -2), 1);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_8)
-{
-    constexpr std::array sit{
-        Situation::Greater_left_argument,
-        Situation::Has_negative_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(-3, -4), -7);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_9)
-{
-    constexpr std::array sit{
-        Situation::Has_negative_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(-4, -3), -7);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_10)
-{
-    constexpr std::array sit{
-        Situation::Has_positive_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(3, 4), 7);
-};
-
-BOOST_AUTO_TEST_CASE(test_case_11)
-{
-    constexpr std::array sit{
-        Situation::Greater_left_argument,
-        Situation::Has_positive_numbers
-    };
-
     BOOST_CHECK_EQUAL(math::add(4, 3), 7);
 };
+
+BOOST_AUTO_TEST_SUITE_END();
+
 
 BOOST_AUTO_TEST_SUITE_END();
--- template/app/test_target/all_lib_target_specs/fn/specs_unitname_fn/specs_unitname_fn.cpp
+++ template/app/test_target/all_lib_target_specs/fn/specs_unitname_fn/specs_unitname_fn.cpp
@@ -1,26 +1,21 @@
 #include <boost/test/unit_test.hpp>
-#include <array>
 
 BOOST_AUTO_TEST_SUITE(unitname_fn);
 
-enum class Situation
-{
-    First,
-    Second,
-    Third,
-    Nth
-};
 
-BOOST_AUTO_TEST_CASE(test_case_1)
-{
-    constexpr std::array sit{
-        Situation::First,
-        Situation::Second,
-        Situation::Third,
-        Situation::Nth
-    };
+BOOST_AUTO_TEST_SUITE(Outcome_1);
 
+BOOST_AUTO_TEST_CASE(Situation_1)
+{
     BOOST_FAIL("Test case body not completed yet.");
 };
+
+BOOST_AUTO_TEST_CASE(Situation_2)
+{
+    BOOST_FAIL("Test case body not completed yet.");
+};
+
+BOOST_AUTO_TEST_SUITE_END();
+
 
 BOOST_AUTO_TEST_SUITE_END();
