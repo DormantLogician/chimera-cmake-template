f2da3a73ff4cd0e0f8da5c4e59733814fbbf3470
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-09-02 15:39:43 -0400

    Update packaging configuration to enable CMake install components.
    This is mainly so we don't include anything in the distribution that
    is not needed in the future.

--- util/build/cmake/setup/packing.cmake
+++ util/build/cmake/setup/packing.cmake
@@ -1,5 +1,8 @@
 include(GNUInstallDirs) # Define common file installation paths.
 
+set(CPACK_COMPONENTS_ALL app)
+set(CPACK_DEB_COMPONENT_INSTALL ON)
+
 set(CPACK_PACKAGE_CONTACT "Full Name <Email>")
 set(CPACK_STRIP_FILES YES) # Strip debug symbols from binary files.
 set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS # Make sure directory permissions are set properly.
@@ -9,9 +12,9 @@ set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS # Make sure directory permission
 )
 
 set(CPACK_SOURCE_GENERATOR "ZIP")
-set(CPACK_SOURCE_IGNORE_FILES /built;/test_package/build;/.git;/.vscode)
+set(CPACK_SOURCE_IGNORE_FILES /built;/build;/test_package/build;/.git;/.vscode)
 
 set(CPACK_GENERATOR "DEB")
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT) # Use Debian naming conventions for binary files.
-set(CPACK_DEBIAN_PACKAGE_SECTION utils)
+set(CPACK_DEBIAN_PACKAGE_SECTION devel)
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES) # Make better dependency list in deb file.
