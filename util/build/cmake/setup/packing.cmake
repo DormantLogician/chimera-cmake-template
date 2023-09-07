1db99a889b177e865a550b564319ebec28ffd9d7
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-09-07 17:40:53 -0400

    Upgrade CPack functionality scripts - also improve error reporting in task scripts.
    This changes how software is packaged so Windows,
    Linux, and OSX are more accounted for.

--- util/build/cmake/setup/packing.cmake
+++ util/build/cmake/setup/packing.cmake
@@ -1,20 +1,24 @@
 include(GNUInstallDirs) # Define common file installation paths.
 
 set(CPACK_COMPONENTS_ALL app)
-set(CPACK_DEB_COMPONENT_INSTALL ON)
+set(CPACK_STRIP_FILES YES) # Strip debug symbols from binary files.
+set(CPACK_PACKAGE_CHECKSUM SHA256)
+set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt)
+set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/dist)
+set(CPACK_THREADS 2)
 
 set(CPACK_PACKAGE_CONTACT "Full Name <Email>")
-set(CPACK_STRIP_FILES YES) # Strip debug symbols from binary files.
+
 set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS # Make sure directory permissions are set properly.
     OWNER_READ OWNER_WRITE OWNER_EXECUTE
     GROUP_READ GROUP_EXECUTE
     WORLD_READ WORLD_EXECUTE
 )
 
-set(CPACK_SOURCE_GENERATOR "ZIP")
+set(CPACK_SOURCE_GENERATOR ZIP)
 set(CPACK_SOURCE_IGNORE_FILES /built;/test_package/build;/.git;/.vscode;/CMakeUserPresets.json)
 
-set(CPACK_GENERATOR "DEB")
+set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT) # Use Debian naming conventions for binary files.
 set(CPACK_DEBIAN_PACKAGE_SECTION utils)
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES) # Make better dependency list in deb file.
